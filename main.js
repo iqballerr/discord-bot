1f592fdadd723f60d3558f3c21beb7c4e215c15a
iqballerr <ilodin@uci.edu>
2021-03-10 01:33:26 -0800

    set up eslint rules

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+// eslint-disable-next-line no-unused-vars
 const dotenv = require('dotenv').config();
 
 
@@ -8,28 +9,31 @@ const prefix = '$';
 
 
 client.once('ready', () => {
-    console.log('Bot Iq is online!');
+	console.log('Bot Iq is online!');
 });
 
 client.on('message', message =>{
-  if(!message.content.startsWith(prefix) || message.author.bot) return;
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).split(/ +/);
-  const command = args.shift().toLowerCase();
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
 
-  if(command == 'help'){
-      message.channel.send("Sorry bud, but I can't help you. Hint: '$commands'");
+	if(command == 'help') {
+		message.channel.send('Sorry bud, but I can\'t help you. Hint: \'$commands\'');
 
-  } else if(command == 'commands'){
-    message.channel.send("Commands: $help, $hello, $iqbal");
+	}
+	else if(command == 'commands') {
+		message.channel.send('Commands: $help, $hello, $iqbal');
 
-  } else if(command == 'hello'){
-    message.channel.send("Hi! Nice to meet you bud :)");
+	}
+	else if(command == 'hello') {
+		message.channel.send('Hi! Nice to meet you bud :)');
 
-  } else if(command == 'iqbal'){
-    message.channel.send("Iqbot");
-  }
-  
+	}
+	else if(command == 'iqbal') {
+		message.channel.send('Iqbot');
+	}
+
 });
 
 const TOKEN = process.env.TOKEN;
