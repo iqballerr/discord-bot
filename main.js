74b5971b135239bfbd3431c39759c6f15ad52327
iqballerr <ilodin@uci.edu>
2021-03-10 00:48:51 -0800

    Added $help command

--- main.js
+++ main.js
@@ -4,11 +4,24 @@ const dotenv = require('dotenv').config();
 
 const client = new Discord.Client();
 
+const prefix = '$';
+
 
 client.once('ready', () => {
     console.log('Bot Iq is online!');
 });
 
+client.on('message', message =>{
+  if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if(command == 'help'){
+      message.channel.send("Sorry bud, but I can't help you");
+  }
+});
+
 const TOKEN = process.env.TOKEN;
 
-client.login(TOKEN)
+client.login(TOKEN);
