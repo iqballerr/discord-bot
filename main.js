2fcb9d009c80562f8ae41248354a95cc0b01c93e
iqballerr <ilodin@uci.edu>
2021-03-10 00:15:02 -0800

    Created bot iq

---
+++ main.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+    console.log('Bot Iq is online!');
+});
+
+
+client.login('ODE5MTE4NzA4ODUyNjU0MDk5.YEh9vg.nUw5aLRWtxJiAVp63AUnGmS3cd8')
